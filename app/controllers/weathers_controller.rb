e55e3a03d45b84158b2c55711e4d68511c776f74
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-27 19:27:34 -0400

    refreshed API key and hid it for security

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,6 +1,6 @@
 class WeathersController < ApplicationController
   def index
-    response = Unirest.get("http://api.wunderground.com/api/14a202a6d9f9e6e8/conditions/q/NY/New_York.json")
+    response = Unirest.get("http://api.wunderground.com/api/#{ENV['API_KEY']}/conditions/q/NY/New_York.json")
     render json: response.body
   end
 end
